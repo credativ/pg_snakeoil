5e62fc059c512392fc21e980e30fe8efbe53166e
Christoph Berg <christoph.berg@credativ.de>
2019-01-28 13:37:42 +0100

    Fix bytea string literal syntax in regression tests

--- sql/pg_snakeoil.sql
+++ sql/pg_snakeoil.sql
@@ -19,7 +19,7 @@ SELECT so_virus_name('Hello World!');
 -- bytea Functions
 -- ------------------------------------------------------------------------
 
-SELECT so_is_infected(E'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'::bytea);
-SELECT so_virus_name(E'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'::bytea);
-SELECT so_is_infected(E'Hello World!'::bytea);
-SELECT so_virus_name(E'Hello World!'::bytea);
+SELECT so_is_infected('X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'::bytea);
+SELECT so_virus_name('X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'::bytea);
+SELECT so_is_infected('Hello World!'::bytea);
+SELECT so_virus_name('Hello World!'::bytea);
